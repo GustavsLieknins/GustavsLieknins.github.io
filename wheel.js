2aafe619167ea2cfe32f743a470e70a42aa1e70c
Jēkabs Krīgerts <jekabs.krigerts@adazuvidusskola.lv>
2023-06-14 00:51:41 +0300

    added

--- wheel.js
+++ wheel.js
@@ -1,7 +1,3 @@
-
-
-
-
 let skaitlis = 0;
 let randoms = Math.floor(Math.random() * 5) + 5;
 document.getElementById("button-for-sending-names").addEventListener("click", wheelMaker);
@@ -10,11 +6,27 @@ document.getElementById("button-for-sending-names").addEventListener("click", wh
 //   }else{
 
 //   }
-for(let i = 0; i<randoms; i++){
-    wheelMaker()
-}
-
+// for(let i = 0; i<randoms; i++){
+//     wheelMaker()
+// }
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+  let looper = getRndInteger(29, 45);
+  let numba = 0;
+  let used = 0;
 function wheelMaker(){
+    if(used == 1){
+        const smoll = document.getElementById("random-name");
+        smoll.remove();
+        used = 0;
+        numba = 0;
+        wheelMaker();
+        skaitlis = 0;
+        looper = getRndInteger(29, 50);
+    }else{
+    numba++
+    console.log(looper);
     console.log("random vertiba" + randoms);
     const inputText = document.getElementById("teksta-logs").value;
     const vardi = inputText.split(" ");
@@ -22,21 +34,47 @@ function wheelMaker(){
     console.log(vardi);
 
     const containers = document.getElementById("random-shower");
-    const length = vardi.length;
+    const length = vardi.length - 1;
 
         const teksts = document.createElement("h1");
-        teksts.className = "random-name";
+        teksts.id = "random-name";
         teksts.textContent = vardi[skaitlis];
-        teksts.style.animation = 'randomiser 0.3s';
+        if(numba >= looper/1.2){
+            teksts.style.animation = 'randomiser 0.18s';
+        }else if(numba >= looper/1.4){
+            teksts.style.animation = 'randomiser 0.1s';
+        }else if(numba >= looper/2){
+            teksts.style.animation = 'randomiser 0.07s';  
+        }else{
+            teksts.style.animation = 'randomiser 0.05s';
+        }
         containers.appendChild(teksts);
-
     teksts.addEventListener('animationend', () => {
-        containers.removeChild(teksts);
-        skaitlis = skaitlis+1;
-        wheelMaker(); // Call the function recursively to add another element
+        if(numba == looper){
+            teksts.style.animation = '';
+            teksts.textContent = vardi[skaitlis];
+            // teksts.style.animation = 'winner 0.3s';
+            containers.appendChild(teksts);
+            used = 1;
+            winner(containers, teksts);
+            
+        }else if(skaitlis == length){
+            containers.removeChild(teksts);
+            skaitlis = 0;
+            wheelMaker();
+        }else{
+            containers.removeChild(teksts);
+            skaitlis = skaitlis+1;
+            wheelMaker();
+        }
       });
     }
-    // Start the animation by adding the first element
+    }
+
+    function winner(containers, teksts){
+        teksts.style.animation = 'winner 1s';
+        containers.appendChild(teksts);  
+    }
 
 
 
@@ -45,25 +83,4 @@ function wheelMaker(){
 
 
 
-/////////////////////////////////////////////////////////////////////////////////
-
-
-    // vardi.forEach((vardii, index) => {
-                // containers.removeChild(teksts);
-        // const teksts = document.createElement("h1");
-        // teksts.className = "random-name";
-        // teksts.textContent = vardi[index];
-        // teksts.style.animation = 'randomiser 0.3s';
-        // containers.appendChild(teksts);
-        // setTimeout(() => {
-        //     console.log("bahh");
-        //   }, 1000);
-
-      
-        // Start the loop by calling the recursive function
-    //     containers.removeChild(teksts);
-
-        
-    // });
-    
 
