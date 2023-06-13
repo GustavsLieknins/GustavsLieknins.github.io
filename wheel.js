74d3d06ba1020b482b0542c82f5874a1aadefec9
Gustavs Lieknins <ipb22.g.lieknins@vtdt.edu.lv>
2023-06-13 10:59:22 -0700

    added

--- wheel.js
+++ wheel.js
@@ -1,8 +1,21 @@
 
 
 
+
+let skaitlis = 0;
+let randoms = Math.floor(Math.random() * 5) + 5;
 document.getElementById("button-for-sending-names").addEventListener("click", wheelMaker);
+// if(skaitlis == length){
+//     skaitlis = 0;
+//   }else{
+
+//   }
+for(let i = 0; i<randoms; i++){
+    wheelMaker()
+}
+
 function wheelMaker(){
+    console.log("random vertiba" + randoms);
     const inputText = document.getElementById("teksta-logs").value;
     const vardi = inputText.split(" ");
 
@@ -11,16 +24,29 @@ function wheelMaker(){
     const containers = document.getElementById("random-shower");
     const length = vardi.length;
 
-    for(let i = 0; i<length; i++){
         const teksts = document.createElement("h1");
         teksts.className = "random-name";
-        teksts.textContent = vardi[i];
+        teksts.textContent = vardi[skaitlis];
         teksts.style.animation = 'randomiser 0.3s';
         containers.appendChild(teksts);
-        //   teksts.textContent = "";
-      
-        // Start the loop by calling the recursive function
+
+    teksts.addEventListener('animationend', () => {
+        containers.removeChild(teksts);
+        skaitlis = skaitlis+1;
+        wheelMaker(); // Call the function recursively to add another element
+      });
     }
+    // Start the animation by adding the first element
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
 
     // vardi.forEach((vardii, index) => {
                 // containers.removeChild(teksts);
@@ -40,4 +66,4 @@ function wheelMaker(){
         
     // });
     
-}
+
